0f16515f854e2c91ab37744a855e2a347e03191c
Michael Wagner <mdwranger@gmail.com>
2023-10-08 09:06:51 -0400

    feat: add specs

--- spec/lucky_hxml_spec.cr
+++ spec/lucky_hxml_spec.cr
@@ -1,8 +1,40 @@
 require "./spec_helper"
 
-describe LuckyHXML do
-  # TODO: Write tests
+include ContextHelper
+
+private class TestComponent < LuckyHXML::Component
+  def render
+    text "Hello World! (from Component)"
+  end
+end
+
+private class TestScreen < LuckyHXML::Screen
+  def render
+    doc do
+      screen do
+        styles do
+          style id: "body", flex: "1", backgroundColor: "white"
+        end
+        body style: "body" do
+          view do
+            text "Hello World! (from Screen)"
+          end
+        end
+      end
+    end
+  end
+end
+
+describe LuckyHXML::Component do
   it "works" do
-    false.should eq(true)
+    render = TestComponent.new(context: build_context).perform_render
+    render.should contain("Hello World! (from Component)")
+  end
+end
+
+describe LuckyHXML::Screen do
+  it "works" do
+    render = TestScreen.new(context: build_context).perform_render
+    render.should contain("Hello World! (from Screen)")
   end
 end
